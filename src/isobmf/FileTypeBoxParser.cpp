978bdbbfe5ff48e2e107117db38cadcb94fbbcfb
Ian Zagorskikh <ianzag@gmail.com>
2021-01-14 22:02:07 +0700

    * Refactor File Type Box parser

--- src/isobmf/FileTypeBoxParser.cpp
+++ src/isobmf/FileTypeBoxParser.cpp
@@ -4,28 +4,31 @@
 
 namespace isobmf {
 
+void FileTypeBoxParser::startParse()
+{
+    switchState(State::MajorBrand);
+}
+
 void FileTypeBoxParser::parseChar(std::uint8_t ch)
 {
     switch (m_state) {
     case State::MajorBrand:
-        if (m_parser.putChar(ch) == 4) {
-            m_majorBrand = m_parser.getAs<std::uint32_t>();
-            m_state = State::MinorVersion;
-            m_parser.reset();
+        if (m_parser.putChar(ch) == sizeof(BrandNumber)) {
+            m_majorBrand = m_parser.getAs<BrandNumber>();
+            switchState(State::MinorVersion);
         }
         break;
     case State::MinorVersion:
-        if (m_parser.putChar(ch) == 4) {
-            m_minorVersion = m_parser.getAs<std::uint32_t>();
-            m_state = State::CompatibleBrands;
-            m_parser.reset();
+        if (m_parser.putChar(ch) == sizeof(VersionNumber)) {
+            m_minorVersion = m_parser.getAs<VersionNumber>();
+            switchState(State::CompatibleBrands);
         }
         break;
     case State::CompatibleBrands:
-        if (m_parser.putChar(ch) == 4) {
-            const auto brandCode = m_parser.getAs<std::uint32_t>();
+        if (m_parser.putChar(ch) == sizeof(BrandNumber)) {
+            const auto brandCode = m_parser.getAs<BrandNumber>();
             m_compatibleBrands.push_back(brandCode);
-            m_parser.reset();
+            switchState(State::CompatibleBrands);
         }
         break;
     }
@@ -35,6 +38,7 @@ std::ostream& FileTypeBoxParser::printDetails(std::ostream& os) const
 {
     os << "majorBrand: " << makeBrandName(m_majorBrand) << " ";
     os << "minorVersion: "  << std::dec << m_minorVersion << " ";
+
     auto numBrands = m_compatibleBrands.size();
     os << "compatibleBrands: {";
     for (const auto brandName : m_compatibleBrands) {
@@ -42,6 +46,7 @@ std::ostream& FileTypeBoxParser::printDetails(std::ostream& os) const
           if (--numBrands) os << " ";
     }
     os << "}";
+
     return os;
 }
 
